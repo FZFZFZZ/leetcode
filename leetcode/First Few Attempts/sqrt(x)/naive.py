a67a087d996c867f27b05d1fa1f174df5bb8075b
FZFZFZZ <69145190+FZFZFZZ@users.noreply.github.com>
2024-12-12 01:39:46 +0800

    Delete leetcode directory

--- leetcode/First Few Attempts/sqrt(x)/naive.py
+++
@@ -1,9 +0,0 @@
-def mySqrt(x):
-    last = 10
-    curr = 100
-    while (abs(last - curr) > 0.5):
-        last = curr
-        curr -= curr / 2 - x / (2 * curr)
-    return int(curr)
-
-print(mySqrt(6))
